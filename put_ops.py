2e25203a9295467da10c9684f222901cdeeda7b7
Debjoty Mitra <debjotyms@gmail.com>
2025-05-07 09:53:10 +0600

    Add post and put methods sample code
    Signed-off-by: Debjoty Mitra <debjotyms@gmail.com>

---
+++ put_ops.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Define model Item
+class Item(BaseModel):
+    name: str
+    description: str
+
+# Define items at application start
+items = {"bananas": "Yellow fruit."}
+
+app = FastAPI()
+
+
+@app.put("/items")
+def update_item(item: Item):
+    name = item.name
+    items[name] = item.description
+    return item
+
+@app.get("/items")
+def show_items():
+    return items
